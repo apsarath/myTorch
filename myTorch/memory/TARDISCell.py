112ebc13667dedeef9260b954b2d1425bbb29b69
chinnadhurai <chinnadhurai@gmail.com>
2017-10-24 01:55:37 -0400

    mini-batched version
    rough

--- myTorch/memory/TARDISCell.py
+++ myTorch/memory/TARDISCell.py
@@ -38,8 +38,7 @@ class TARDISCell(nn.Module):
         self.b_c = nn.Parameter(torch.Tensor(hidden_size))
 
 	# memory related parameters
-	self.read_loc_list = [self._variable(torch.zeros((num_mem_cells)))]
-	#self.num_cells_written_so_far = torch.IntTensor(1).zero_()
+	self.read_loc_list = None
 	self.num_cells_written_so_far = 0
 
 	self.W_m2i = nn.Parameter(torch.Tensor(micro_state_size, hidden_size))
@@ -76,62 +75,72 @@ class TARDISCell(nn.Module):
 	return v
 
     def forward(self, input, last_hidden):
+	batch_size = input.size()[0]
+
+	if not self.read_loc_list:
+	    self.read_loc_list = [self._variable(torch.zeros((batch_size, self.num_mem_cells)))]
+	if "mem" not in last_hidden:
+	    last_hidden["mem"] = self._variable(torch.zeros(batch_size, self.num_mem_cells, self.micro_state_size))
+
 	# compute read weights
         last_hidden_micro_state = torch.mm(last_hidden["h"], self.W_m) + self.b_m
+	last_hidden_micro_state = torch.unsqueeze(last_hidden_micro_state, 2)
 
         # Compute read vector
-        m_logits = torch.mm(torch.mm(last_hidden_micro_state, torch.t(last_hidden["mem"])), self.W_g_m)
+	m_logits = torch.mm(torch.squeeze(torch.bmm(last_hidden["mem"], last_hidden_micro_state), 2), self.W_g_m)
 	h_logits = torch.mm(last_hidden["h"], self.W_g_h)
 	x_logits = torch.mm(input, self.W_g_x)
-	usage_vector = torch.sum(torch.stack(self.read_loc_list, 0),0, keepdim=True)
+	usage_vector = torch.sum(torch.stack(self.read_loc_list, 1), 1)
 	usage_vector = torch.nn.functional.softmax(usage_vector).view_as(usage_vector)
 	u_logits = torch.mm(usage_vector, self.W_g_u)
 	logits = m_logits + h_logits + x_logits + u_logits
 
         sampled_one_hot_location = gumbel_softmax(logits, use_gpu=self.use_gpu)
-	self.read_loc_list.append(sampled_one_hot_location.squeeze().detach().data)
-        sampled_mirco_state = torch.mm(sampled_one_hot_location, last_hidden["mem"])
-        sampled_location = torch.max(sampled_one_hot_location, 1)[1]
+	self.read_loc_list.append(sampled_one_hot_location.detach().data)
+        sampled_mirco_state = torch.bmm(torch.unsqueeze(sampled_one_hot_location, 1), last_hidden["mem"])
+	sampled_micro_state = torch.squeeze(sampled_mirco_state, 1)
 
         self.W_i = torch.cat((self.W_x2i, self.W_h2i, self.W_c2i, self.W_m2i), 0)
         self.W_f = torch.cat((self.W_x2f, self.W_h2f, self.W_c2f, self.W_m2f), 0)
         self.W_o = torch.cat((self.W_x2o, self.W_h2o, self.W_c2o, self.W_m2o), 0)
 
-	alpha = torch.mm(last_hidden["h"], self.w_a_h) + torch.mm(input, self.w_a_x) + torch.mm(sampled_mirco_state, self.w_a_r)
+	alpha = torch.mm(last_hidden["h"], self.w_a_h) + torch.mm(input, self.w_a_x) + torch.mm(sampled_micro_state, self.w_a_r)
         alpha = gumbel_sigmoid(alpha, 0.3, use_gpu=self.use_gpu).repeat(last_hidden["h"].size())
-        beta = torch.mm(last_hidden["h"], self.w_b_h) + torch.mm(input, self.w_b_x) + torch.mm(sampled_mirco_state, self.w_b_r)
+        beta = torch.mm(last_hidden["h"], self.w_b_h) + torch.mm(input, self.w_b_x) + torch.mm(sampled_micro_state, self.w_b_r)
         beta = gumbel_sigmoid(beta, 0.3, use_gpu=self.use_gpu).repeat(last_hidden["h"].size())
 
-        c_input = torch.cat((input, last_hidden["h"], last_hidden["c"], sampled_mirco_state), 1)
+        c_input = torch.cat((input, last_hidden["h"], last_hidden["c"], sampled_micro_state), 1)
         i = torch.sigmoid(torch.mm(c_input, self.W_i) + self.b_i)
         f = torch.sigmoid(torch.mm(c_input, self.W_f) + self.b_f)
 
-	cp = torch.mm(input, self.W_x2c) + alpha * torch.mm(last_hidden["h"], self.W_h2c) + beta * torch.mm(sampled_mirco_state, self.W_m2c) + self.b_c
+	cp = torch.mm(input, self.W_x2c) + alpha * torch.mm(last_hidden["h"], self.W_h2c) + beta * torch.mm(sampled_micro_state, self.W_m2c) + self.b_c
 	cp = torch.tanh(cp)
         c = f * last_hidden["c"] + i * cp
 
-        o_input = torch.cat((input, last_hidden["h"], c, sampled_mirco_state), 1)
+	o_input = torch.cat((input, last_hidden["h"], c, sampled_micro_state), 1)
         o = torch.sigmoid(torch.mm(o_input, self.W_o) + self.b_o)
 
         h = o * torch.tanh(c)
 
         # writing into memory
         curr_micro_state = torch.mm(h, self.W_m) + self.b_m
+	curr_micro_state =  torch.unsqueeze(curr_micro_state, 1)
 
 	def one_hot(num):
-	   o = np.zeros((self.num_mem_cells,1))
-	   o[num] = 1
+	   o = np.zeros((batch_size, self.num_mem_cells,1))
+	   o[:, num] = 1
 	   return o
 
 	if self.num_cells_written_so_far < self.num_mem_cells:
 	   one_hot_mask = self._variable(torch.Tensor(one_hot(self.num_cells_written_so_far)))
 	   inverse_hot_mask = self._variable(torch.ones(one_hot_mask.size())) - one_hot_mask
-	   memory_matrix = curr_micro_state.repeat(self.num_mem_cells,1) * one_hot_mask + last_hidden["mem"] * inverse_hot_mask
            self.num_cells_written_so_far += 1
 	else:
-	   one_hot_mask = sampled_one_hot_location.view(-1,1)
+	   one_hot_mask = torch.unsqueeze(sampled_one_hot_location, -1)
 	   inverse_hot_mask = self._variable(torch.ones(one_hot_mask.size())) - one_hot_mask
-	   memory_matrix = curr_micro_state.repeat(self.num_mem_cells,1) * one_hot_mask + last_hidden["mem"] * inverse_hot_mask
+	
+	memory_matrix = curr_micro_state.repeat(1,self.num_mem_cells,1) * one_hot_mask + last_hidden["mem"] * inverse_hot_mask
+
         hidden = {}
         hidden["h"] = h
         hidden["c"] = c
@@ -142,7 +151,6 @@ class TARDISCell(nn.Module):
         hidden = {}
         hidden["h"] = self._variable(torch.Tensor(np.zeros((1, self.hidden_size))))
         hidden["c"] = self._variable(torch.Tensor(np.zeros((1, self.hidden_size))))
-	hidden["mem"] = self._variable(torch.zeros(self.num_mem_cells, self.micro_state_size))
         return hidden
 
     def reset_parameters(self):
