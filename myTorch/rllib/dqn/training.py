e2df15c76fbd1c74cdbd351e96b8a35e3f5355c3
chinnadhurai <chinnadhurai@gmail.com>
2018-01-25 19:06:45 -0500

    Make Double dqn optional

--- myTorch/rllib/dqn/training.py
+++ myTorch/rllib/dqn/training.py
@@ -64,6 +64,7 @@ def train_dqn_agent():
 			 		numpy_rng,
 			 		discount_rate=config.discount_rate, 
 			 		grad_clip = [config.grad_clip_min, config.grad_clip_max], 
+					is_double_dqn = config.is_double_dqn,
 			 		target_net_soft_update=config.target_net_soft_update,
 			 		target_net_update_freq=config.target_net_update_freq,
 			 		target_net_update_fraction=config.target_net_update_fraction,
