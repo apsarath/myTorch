1ec30a5596c0cd23088df4bc7f41ed9dec45dda0
chinnadhurai <chinnadhurai@gmail.com>
2018-04-28 18:35:40 -0400

    state aggregation changes

---
+++ myTorch/projects/bottleneck/construct_state_agg_replay_buffer.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+
+import os
+import math
+import numpy as np
+import argparse
+import time
+
+import torch
+
+import myTorch
+from myTorch.environment import make_environment
+from myTorch.utils import modify_config_params, one_hot, RLExperiment, get_optimizer, load_w2v_vectors, GenExperiment
+from myTorch.rllib.dqn.q_networks import *
+
+
+from sklearn.cluster import KMeans
+from myTorch.projects.bottleneck.config import *
+from myTorch.projects.bottleneck.mdp_classifier import MDPCLassifier
+from myTorch.rllib.dqn import ReplayBuffer, DQNAgent
+from myTorch.utils import MyContainer
+from myTorch.utils.logging import Logger
+
+parser = argparse.ArgumentParser(description="MDP Training")
+parser.add_argument('--config', type=str, default="home_world", help="config name")
+parser.add_argument('--base_dir', type=str, default=None, help="base directory")
+parser.add_argument('--config_params', type=str, default="default", help="config params to change")
+parser.add_argument('--exp_desc', type=str, default="default", help="additional desc of exp")
+parser.add_argument('--run_num', type=int, default=1, help="exp run number")
+args = parser.parse_args()
+
+def train_mdp():
+    assert(args.base_dir)
+    config = eval(args.config)()
+    if args.config_params != "default":
+        modify_config_params(config, args.config_params)
+
+    train_dir = os.path.join(args.base_dir, config.train_dir, config.exp_name, config.env_name, 
+        "{}__{}".format(args.config_params, args.exp_desc))
+
+    torch.manual_seed(config.seed)
+    numpy_rng = np.random.RandomState(seed=config.seed)
+
+    experiment = RLExperiment(config.exp_name, train_dir, config.backup_logger)
+    replay_buffer = ReplayBuffer(numpy_rng, size=config.replay_buffer_size, compress=config.replay_compress)
+    experiment.register_replay_buffer(replay_buffer)
+
+    env = make_environment(config.env_name)
+    env.seed(seed=config.seed)
+    experiment.register_env(env)
+
+    assert (experiment.is_resumable("current"))
+    print("resuming the experiment...")
+    experiment.resume("current")
+
+    # Modify action dtype
+    for d_id in range(len(replay_buffer.data["actions"])):
+        if replay_buffer.data["actions"][d_id] is not None:
+            replay_buffer.data["actions"][d_id] = replay_buffer.data["actions"][d_id].astype(np.uint8)
+
+    # mdp experiment stuff
+    mdp_train_dir = os.path.join(train_dir, "mdp_{}".format(args.run_num))
+    mdp_experiment = GenExperiment(config.exp_name, mdp_train_dir, config.backup_logger)
+
+    classifier = {}
+    state_agg_replay_buffer = {}
+    for num_clusters in [4,12,16,24,32,8]:
+        # cluster them
+        print(("Loading classifier with cluster size : ... {}".format(num_clusters)))
+
+        # create classifier 
+        classifier[num_clusters] = MDPCLassifier(env.action_dim, env.obs_dim, num_clusters, use_gpu=config.use_gpu,
+                                                grad_clip=[config.grad_clip_min, config.grad_clip_max])
+        if config.use_gpu:
+            classifier[num_clusters].cuda()
+        mdp_experiment.register("classifier_{}".format(num_clusters), classifier[num_clusters])
+
+        # load best classifier
+        print("Loading Classifier {}".format(num_clusters))
+        mdp_experiment.resume("best_model", input_obj_tag="classifier_{}".format(num_clusters))
+
+        # Create new samples and create a new replay buffer.
+        state_agg_replay_buffer[num_clusters] = ReplayBuffer(numpy_rng, size=config.replay_buffer_size, compress=config.replay_compress)
+        mdp_experiment.register("state_agg_replay_buffer_{}".format(num_clusters), state_agg_replay_buffer[num_clusters])
+        
+        # Copy stuff from the old replay buffer into the new one.
+        for key in replay_buffer.data:
+            state_agg_replay_buffer[num_clusters].data[key] = replay_buffer.data[key]
+    
+        
+
+        start_time = time.time()
+        for d_id in range(len(replay_buffer.data["observations"])):
+            if replay_buffer.data["observations"][d_id] is None:
+                continue
+
+            # get obs_ids
+            _, obs_cluster_id, _ = classifier[num_clusters].predict_cluster_id_rewards({
+                    "obs" : torch.from_numpy(replay_buffer.data["observations"][d_id][1]).type(torch.LongTensor).cuda(),
+                    "actions": torch.from_numpy(replay_buffer.data["actions"][d_id]).type(torch.FloatTensor).cuda()})
+
+            _, obs_tp1_cluster_id, _ = classifier[num_clusters].predict_cluster_id_rewards({
+                    "obs" : torch.from_numpy(replay_buffer.data["observations_tp1"][d_id][1]).type(torch.LongTensor).cuda(),
+                    "actions": torch.from_numpy(replay_buffer.data["actions"][d_id]).type(torch.FloatTensor).cuda()})
+
+
+            obs_cluster_id = obs_cluster_id.cpu().numpy()[0]
+            obs_tp1_cluster_id = obs_tp1_cluster_id.cpu().numpy()[0]
+            state_agg_replay_buffer[num_clusters].data["observations"][d_id][1].fill(0)
+            state_agg_replay_buffer[num_clusters].data["observations"][d_id][1][obs_cluster_id] = 1
+
+            state_agg_replay_buffer[num_clusters].data["observations_tp1"][d_id][1].fill(0)
+            state_agg_replay_buffer[num_clusters].data["observations_tp1"][d_id][1][obs_cluster_id] = 1
+
+        print(("Done in {} secs".format(time.time()-start_time)))
+
+        mdp_experiment.save("best_model", input_obj_tag="state_agg_replay_buffer_{}".format(num_clusters))
+
+def format_legal_moves(legal_moves, action_dim):
+
+    new_legal_moves = np.zeros(action_dim) - float("inf")
+    if len(legal_moves) > 0:
+        new_legal_moves[legal_moves] = 0
+    return new_legal_moves
+
+def _analyze_clusters(cluster_ids, num_clusters):
+    hist = {}
+    for idx in range(num_clusters):
+        hist[idx] = len([i for i in cluster_ids if i == idx])
+    print(("Cluster hist : \n{}\n".format(hist)))
+
+
+if __name__=="__main__":
+    train_mdp()
--- myTorch/projects/bottleneck/mdp_classifier.py
+++ myTorch/projects/bottleneck/mdp_classifier.py
@@ -7,131 +7,132 @@ from myTorch.utils import my_variable
 from myTorch.memory import RNNCell, GRUCell, LSTMCell, TARDISCell
 
 class MDPCLassifier(nn.Module):
-	def __init__(self, action_dim, obs_dim, num_clusters, num_rewards=1, use_gpu=False, rnn_type="LSTM", grad_clip=[None, None]):
-		super(self.__class__, self).__init__()
-	
-		self._use_gpu = use_gpu
-		self._action_dim = action_dim
-		self._obs_dim = obs_dim
-		self._num_clusters = num_clusters
-		self._num_rewards = num_rewards
-		self._rnn_hidden_size = 256
-		self._embedding_dim = 20
-		self._rnn_input_size = self._embedding_dim
-		self._max_vocab_size = 100
-		self._max_num_steps = 30
-		self._pad = 0
-		self._optimizer = None
-		self._grad_clip = grad_clip
+    def __init__(self, action_dim, obs_dim, num_clusters, num_rewards=1, use_gpu=False, rnn_type="LSTM", grad_clip=[None, None]):
+        super(self.__class__, self).__init__()
+    
+        self._use_gpu = use_gpu
+        self._action_dim = action_dim
+        self._obs_dim = obs_dim
+        self._num_clusters = num_clusters
+        self._num_rewards = num_rewards
+        self._rnn_hidden_size = 256
+        self._embedding_dim = 20
+        self._rnn_input_size = self._embedding_dim
+        self._max_vocab_size = 100
+        self._max_num_steps = 30
+        self._pad = 0
+        self._optimizer = None
+        self._grad_clip = grad_clip
 
-		self._rnn_type = rnn_type
-		rnn_cell = LSTMCell if self._rnn_type== "LSTM" else GRUCell
-		self._obs_cell = rnn_cell(input_size=self._rnn_input_size, hidden_size=self._rnn_hidden_size, use_gpu=self._use_gpu)
-		self._rnn = { "obs" : self._obs_cell }
+        self._rnn_type = rnn_type
+        rnn_cell = LSTMCell if self._rnn_type== "LSTM" else GRUCell
+        self._obs_cell = rnn_cell(input_size=self._rnn_input_size, hidden_size=self._rnn_hidden_size, use_gpu=self._use_gpu)
+        self._rnn = { "obs" : self._obs_cell }
 
 
-		self._input_embedding = nn.Embedding(self._max_vocab_size, self._embedding_dim, sparse=False, padding_idx=self._pad)
-		self._action_emb = nn.Linear(self._action_dim, 64)
-		self._fc1 = nn.Linear(self._rnn_hidden_size, 64)
-		self._fc2 = nn.Linear(2*64, 64)
-		self._fc3 = nn.Linear(2*64, 64)
+        self._input_embedding = nn.Embedding(self._max_vocab_size, self._embedding_dim, sparse=False, padding_idx=self._pad)
+        self._action_emb = nn.Linear(self._action_dim, 64)
+        self._fc1 = nn.Linear(self._rnn_hidden_size, 64)
+        self._fc2 = nn.Linear(2*64, 64)
+        self._fc3 = nn.Linear(2*64, 64)
 
-		self._fc4 = nn.Linear(64, self._num_clusters)
-		self._fc5 = nn.Linear(64, self._num_rewards)
-		self._hidden = None
+        self._fc4 = nn.Linear(64, self._num_clusters)
+        self._fc5 = nn.Linear(64, self._num_rewards)
+        self._hidden = None
 
-		self._reward_loss = nn.MSELoss()
-		self._cluster_loss = nn.CrossEntropyLoss()
+        self._reward_loss = nn.MSELoss()
+        self._cluster_loss = nn.CrossEntropyLoss()
 
-	def reset_hidden(self, batch_size):
-		self._hidden = {}
-		for k in self._rnn:
-			self._hidden[k] = {}
-			self._hidden[k]["h"] = my_variable(torch.zeros(batch_size, self._rnn_hidden_size), use_gpu=self._use_gpu)
-			if self._rnn_type == "LSTM":
-				self._hidden[k]["c"] = my_variable(torch.zeros(batch_size, self._rnn_hidden_size), use_gpu=self._use_gpu)
+    def reset_hidden(self, batch_size):
+        self._hidden = {}
+        for k in self._rnn:
+            self._hidden[k] = {}
+            self._hidden[k]["h"] = my_variable(torch.zeros(batch_size, self._rnn_hidden_size), use_gpu=self._use_gpu)
+            if self._rnn_type == "LSTM":
+                self._hidden[k]["c"] = my_variable(torch.zeros(batch_size, self._rnn_hidden_size), use_gpu=self._use_gpu)
 
-	def _encode(self, input, data_type):
-		self.reset_hidden(input.shape[0])
-		hidden_next = self._hidden[data_type]
-		encoded_emb = hidden_next['h']
-		input_mask = input.eq(self._pad).type(torch.cuda.FloatTensor)
-		invert_mask = 1 - input_mask
-		input_emb = self._input_embedding(input)
-		for i in range(self._max_num_steps):
-			hidden_next = self._rnn[data_type](input_emb[:,i,:], hidden_next)
-			encoded_emb = (invert_mask[:,i]).unsqueeze(1)*hidden_next['h'] + input_mask[:, i].unsqueeze(1)*encoded_emb
-		return encoded_emb
-		
+    def _encode(self, input, data_type):
+        self.reset_hidden(input.shape[0])
+        hidden_next = self._hidden[data_type]
+        encoded_emb = hidden_next['h']
+        input_mask = input.eq(self._pad).type(torch.cuda.FloatTensor)
+        invert_mask = 1 - input_mask
+        input_emb = self._input_embedding(input)
+        for i in range(self._max_num_steps):
+            hidden_next = self._rnn[data_type](input_emb[:,i,:], hidden_next)
+            encoded_emb = (invert_mask[:,i]).unsqueeze(1)*hidden_next['h'] + input_mask[:, i].unsqueeze(1)*encoded_emb
+        return encoded_emb
+        
 
-	def predict_cluster_id_rewards(self, inputs):
-		obs, actions = inputs["obs"], inputs["actions"]
-		if len(obs.shape) < 2:
-			obs = obs.unsqueeze(0)
-		if len(actions.shape) < 2:
-			actions = actions.unsqueeze(0)
+    def predict_cluster_id_rewards(self, inputs):
+        obs, actions = inputs["obs"], inputs["actions"]
+        if len(obs.shape) < 2:
+            obs = obs.unsqueeze(0)
+        if len(actions.shape) < 2:
+            actions = actions.unsqueeze(0)
  
-		obs_emb = self._fc1(self._encode(obs, "obs"))
-		action_emb = self._action_emb(actions)
-		common_repr = torch.cat((obs_emb, action_emb), dim=1)
+        obs_emb = self._fc1(self._encode(obs, "obs"))
+        action_emb = self._action_emb(actions)
+        common_repr = torch.cat((obs_emb, action_emb), dim=1)
 
-		cluster_logits = self._fc4(F.relu(self._fc2(common_repr)))
-		#cluster_pvals = torch.nn.functional.softmax(cluster_logits, dim=1)
-		cluster_ids = torch.max(cluster_logits, dim=1)[1]
+        cluster_logits = self._fc4(F.relu(self._fc2(common_repr)))
+        #cluster_pvals = torch.nn.functional.softmax(cluster_logits, dim=1)
+        cluster_ids = torch.max(cluster_logits, dim=1)[1]
 
-		reward_logits = self._fc5(F.relu(self._fc3(common_repr)))
-		#reward_pvals = torch.nn.functional.softmax(reward_logits, dim=1)
-		#rewards = torch.max(reward_pvals, dim=1)[1]
-		
-		return cluster_logits, cluster_ids, reward_logits
-		#return cluster_pvals, cluster_ids.data.cpu().numpy(), reward_pvals, rewards.data.cpu().numpy()
+        reward_logits = self._fc5(F.relu(self._fc3(common_repr)))
+        #reward_pvals = torch.nn.functional.softmax(reward_logits, dim=1)
+        #rewards = torch.max(reward_pvals, dim=1)[1]
+        
+        return cluster_logits, cluster_ids, reward_logits
+        #return cluster_pvals, cluster_ids.data.cpu().numpy(), reward_pvals, rewards.data.cpu().numpy()
 
-	def get_params(self):
-		return self.state_dict()
+    def get_params(self):
+        return self.state_dict()
 
-	def set_params(self, state_dict):
-		self.load_state_dict(state_dict)
+    def set_params(self, state_dict):
+        self.load_state_dict(state_dict)
 
-	@property
-	def use_gpu(self):
-		return self._use_gpu
+    @property
+    def use_gpu(self):
+        return self._use_gpu
 
-	def compute_loss(self, minibatch, optimizer, is_training):
-		if self._optimizer is None:
-			self._optimizer = optimizer
+    def compute_loss(self, minibatch, optimizer, is_training):
+        if self._optimizer is None:
+            self._optimizer = optimizer
 
-		if is_training: optimizer.zero_grad()
-		cluster_logits, cluster_ids, reward_logits = self.predict_cluster_id_rewards(minibatch)
-		reward_loss = self._reward_loss(reward_logits, minibatch["rewards"])
-		cluster_loss = self._cluster_loss(cluster_logits, minibatch["cluster_ids"])
-		total_loss = 10*reward_loss + 10*cluster_loss
+        if is_training: optimizer.zero_grad()
+        cluster_logits, cluster_ids, reward_logits = self.predict_cluster_id_rewards(minibatch)
+        reward_loss = self._reward_loss(reward_logits, minibatch["rewards"])
+        cluster_loss = self._cluster_loss(cluster_logits, minibatch["cluster_ids"])
+        total_loss = 10*reward_loss + 10*cluster_loss
 
-		cluster_accuracy = torch.mean(torch.eq(cluster_ids, minibatch["cluster_ids"]).type(torch.cuda.FloatTensor))*100
-		cluster_accuracy = cluster_accuracy.data.cpu().numpy()[0]
-		if not is_training:
-			return cluster_accuracy, total_loss.data.cpu().numpy()[0]
-			#return total_loss.data.cpu().numpy()[0]
+        cluster_accuracy = torch.mean(torch.eq(cluster_ids, minibatch["cluster_ids"]).type(torch.cuda.FloatTensor))*100
+        cluster_accuracy = cluster_accuracy.data.cpu().numpy()[0]
+        if not is_training:
+            return cluster_accuracy, total_loss.data.cpu().numpy()[0]
+            #return total_loss.data.cpu().numpy()[0]
 
-		total_loss.backward()
-		#print "train reward loss : {}, cluster loss : {}, acc : {}".format(reward_loss.data.cpu().numpy()[0], cluster_loss.data.cpu().numpy()[0], cluster_accuracy)
+        total_loss.backward()
+        #print "train reward loss : {}, cluster loss : {}, acc : {}".format(reward_loss.data.cpu().numpy()[0], cluster_loss.data.cpu().numpy()[0], cluster_accuracy)
 
-		if self._grad_clip[0] is not None:
-			for param in self.parameters():
-				param.grad.data.clamp_(self._grad_clip[0], self._grad_clip[1])
+        if self._grad_clip[0] is not None:
+            for param in self.parameters():
+                param.grad.data.clamp_(self._grad_clip[0], self._grad_clip[1])
 
-		optimizer.step()
-		return total_loss
+        optimizer.step()
+        return total_loss
 
-	def load(self, folder_name):
-		fname = os.path.join(folder_name, "net.p")
-		self.load_state_dict(torch.load(fname))
+    def load(self, folder_name):
+        fname = os.path.join(folder_name, "net.p")
+        self.load_state_dict(torch.load(fname))
 
-		fname = os.path.join(folder_name, "optimizer.p")
-		self._optimizer.load_state_dict(torch.load(fname))
+        if self._optimizer:
+            fname = os.path.join(folder_name, "optimizer.p")
+            self._optimizer.load_state_dict(torch.load(fname))
 
-	def save(self, folder_name):
-		fname = os.path.join(folder_name, "net.p")
-		torch.save(self.state_dict(), fname)
+    def save(self, folder_name):
+        fname = os.path.join(folder_name, "net.p")
+        torch.save(self.state_dict(), fname)
 
-		fname = os.path.join(folder_name, "optimizer.p")
-		torch.save(self._optimizer.state_dict(), fname)
+        fname = os.path.join(folder_name, "optimizer.p")
+        torch.save(self._optimizer.state_dict(), fname)
