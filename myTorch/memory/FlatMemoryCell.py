6b1fd4ab80f4d8673fd5eb9c5de288ab61ce6357
chinnadhurai <chinnadhurai@gmail.com>
2018-04-29 16:53:00 -0400

    merge conflict

--- myTorch/memory/FlatMemoryCell.py
+++ myTorch/memory/FlatMemoryCell.py
@@ -23,61 +23,44 @@ class FlatMemoryCell(nn.Module):
         elif activation == "sigmoid":
             self.activation = torch.sigmoid 
         
-        self.W_i2h = nn.Parameter(torch.Tensor(input_size, hidden_size))
-        self.W_h2h = nn.Parameter(torch.Tensor(hidden_size, hidden_size))
-        self.b_h = nn.Parameter(torch.Tensor(hidden_size))
-
-        self.reset_parameters()
-
         self.memory_size = memory_size
         self.k = k
         assert math.sqrt(self.memory_size*self.k).is_integer()
         sqrt_memk = int(math.sqrt(self.memory_size*self.k))
-        self.hm2v_alpha = nn.Linear(self.memory_size + hidden_size, sqrt_memk)
-        self.hm2v_beta = nn.Linear(self.memory_size + hidden_size, sqrt_memk)
+        self.hm2v_alpha = nn.Linear(self.memory_size + hidden_size, 2 * sqrt_memk)
+        self.hm2v_beta = nn.Linear(self.memory_size + hidden_size, 2 * sqrt_memk)
         self.hm2alpha = nn.Linear(self.memory_size + hidden_size, self.k)
         self.hm2beta = nn.Linear(self.memory_size + hidden_size, self.k)
 
-        self.hm2h = nn.Linear(self.memory_size+hidden_size, hidden_size)
+        self.hmi2h = nn.Linear(self.memory_size+hidden_size+self.input_size, hidden_size)
 
         
     def forward(self, input, last_hidden):
-        
-        
-        c_input = torch.cat((input, last_hidden["h_internal"]), 1)
-        W_h = torch.cat((self.W_i2h, self.W_h2h), 0)
-        pre_hidden = torch.add(torch.mm(c_input, W_h), self.b_h)
-        h = self.activation(pre_hidden)
+        c_input = torch.cat((input, last_hidden["h"], last_hidden["memory"]), 1)
+        h = self.activation(self.hmi2h(c_input))
 
         # Flat memory equations
-        alpha = self.hm2alpha(torch.cat((last_hidden["h_internal"],last_hidden["memory"]),1))
-        beta = self.hm2beta(torch.cat((last_hidden["h_internal"],last_hidden["memory"]),1))
+        alpha = self.hm2alpha(torch.cat((h,last_hidden["memory"]),1)).clone()
+        beta = self.hm2beta(torch.cat((h,last_hidden["memory"]),1)).clone()
 
-        v_alpha = self.hm2v_alpha(torch.cat((last_hidden["h_internal"],last_hidden["memory"]),1))
-        v_alpha = torch.bmm(v_alpha.unsqueeze(2), v_alpha.unsqueeze(1)).view(-1, self.k, self.memory_size)
-        phi_alpha = torch.nn.functional.cosine_similarity(v_alpha, last_hidden["memory"].expand_as(v_alpha), dim=-1)
-        add_memory = (phi_alpha*alpha).unsqueeze(2)*v_alpha
+        u_alpha = self.hm2v_alpha(torch.cat((h,last_hidden["memory"]),1)).chunk(2,dim=1)
+        v_alpha = torch.bmm(u_alpha[0].unsqueeze(2), u_alpha[1].unsqueeze(1)).view(-1, self.k, self.memory_size)
+        add_memory = alpha.unsqueeze(2)*v_alpha
 
-        v_beta = self.hm2v_beta(torch.cat((last_hidden["h_internal"],last_hidden["memory"]),1))
-        v_beta = torch.bmm(v_beta.unsqueeze(2), v_beta.unsqueeze(1)).view(-1, self.k, self.memory_size)
-        phi_beta = torch.nn.functional.cosine_similarity(v_beta, last_hidden["memory"].expand_as(v_beta), dim=-1)
-        forget_memory = (phi_beta*beta).unsqueeze(2)*v_beta
+        u_beta = self.hm2v_beta(torch.cat((h,last_hidden["memory"]),1)).chunk(2, dim=1)
+        v_beta = torch.bmm(u_beta[0].unsqueeze(2), u_beta[1].unsqueeze(1)).view(-1, self.k, self.memory_size)
+        forget_memory = beta.unsqueeze(2)*v_beta
 
         hidden = {}
-        hidden["h_internal"] = h
         hidden["memory"] = last_hidden["memory"] + torch.sum(add_memory - forget_memory, dim=1)
-        hidden["h"] = self.hm2h(torch.cat((hidden["h_internal"],hidden["memory"]),1))
+        hidden["h"] = h
         return hidden
 
-    def reset_hidden(self):
+    def reset_hidden(self, batch_size):
         hidden = {}
-        hidden["h_internal"] = torch.Tensor(np.zeros((1, self.hidden_size))).to(self._device)
-        hidden["memory"] = torch.Tensor(np.zeros((1, self.memory_size))).to(self._device)
-
+        hidden["h"] = Variable(torch.Tensor(np.zeros((batch_size, self.hidden_size))))
+        hidden["memory"] = Variable(torch.Tensor(np.zeros((batch_size, self.memory_size))))
+        if self.use_gpu==True:
+            hidden["h"] = hidden["h"].cuda()
+            hidden["memory"] = hidden["memory"].cuda()
         return hidden
-
-    def reset_parameters(self):
-
-        nn.init.xavier_normal(self.W_i2h, gain=nn.init.calculate_gain(act_name(self.activation)))
-        nn.init.orthogonal(self.W_h2h)
-        nn.init.constant(self.b_h, 0)
