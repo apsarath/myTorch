b3fa59b39ba4e5a042974889954e3cd817f815d4
Shagun Sodhani <sshagunsodhani@gmail.com>
2018-05-07 14:52:06 -0400

    Added a new ExpandableLSTM with support for Net2Net operations (for increasing the width of the LSTM cell

--- myTorch/projects/overfeeding/recurrent_net.py
+++ myTorch/projects/overfeeding/recurrent_net.py
@@ -1,11 +1,15 @@
 """Implementation of a generic Recurrent Network."""
 import os
+
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from myTorch.memory import RNNCell, GRUCell, LSTMCell
 from myTorch.memnets.FlatMemoryCell import FlatMemoryCell
+from myTorch.memory import RNNCell, GRUCell
+from myTorch.projects.overfeeding.utils.ExpandableLSTMCell import ExpandableLSTMCell
+from myTorch.projects.overfeeding.utils.net2net import make_h_wider, make_weight_wider_at_input
 
 
 class Recurrent(nn.Module):
@@ -14,7 +18,7 @@ class Recurrent(nn.Module):
     def __init__(self, device, input_size, output_size, num_layers=1, layer_size=[10],
                  cell_name="LSTM", activation="tanh", output_activation="linear"):
         """Initializes a recurrent network."""
-        
+
         super(Recurrent, self).__init__()
 
         self._device = device
@@ -30,7 +34,7 @@ class Recurrent(nn.Module):
 
         self._add_cell(self._input_size, self._layer_size[0])
         for i in range(1, num_layers):
-            self._add_cell(self._layer_size[i-1], self._layer_size[i])
+            self._add_cell(self._layer_size[i - 1], self._layer_size[i])
 
         self._list_of_modules = nn.ModuleList(self._Cells)
 
@@ -56,12 +60,12 @@ class Recurrent(nn.Module):
         Returns:
             model output for current time step.
         """
-        
+
         h = []
         h.append(self._Cells[0](input, self._h_prev[0]))
         for i, cell in enumerate(self._Cells):
             if i != 0:
-                h.append(cell(h[i-1]["h"], self._h_prev[i]))
+                h.append(cell(h[i - 1]["h"], self._h_prev[i]))
         output = torch.add(torch.mm(h[-1]["h"], self._W_h2o), self._b_o)
         if self._output_activation_fn is not None:
             output = self._output_activation_fn(output)
@@ -74,6 +78,19 @@ class Recurrent(nn.Module):
         self._h_prev = []
         for cell in self._Cells:
             self._h_prev.append(cell.reset_hidden(batch_size))
+
+        return self._h_prev
+
+    def reset_hidden_randomly(self, batch_size):
+        """Resets the hidden state randomly for testing the `make_cell_wider` operation.."""
+
+        self._h_prev = []
+        for cell in self._Cells:
+            self._h_prev.append(cell.reset_hidden_randomly(batch_size))
+        return self._h_prev
+
+    def set_hidden(self, h):
+        self._h_prev = h
 
     def _reset_parameters(self):
         """Initializes the parameters."""
@@ -102,7 +119,7 @@ class Recurrent(nn.Module):
             self._Cells.append(RNNCell(self._device, input_size, hidden_size,
                                        activation=self._activation))
         elif self._cell_name == "LSTM":
-            self._Cells.append(LSTMCell(self._device, input_size, hidden_size))
+            self._Cells.append(ExpandableLSTMCell(self._device, input_size, hidden_size))
         elif self._cell_name == "GRU":
             self._Cells.append(GRUCell(self._device, input_size, hidden_size))
         elif self._cell_name == "FlatMemory":
@@ -140,3 +157,42 @@ class Recurrent(nn.Module):
             num_params += p.data.view(-1).size(0)
         print("Num_params : {} ".format(num_params))
         return num_params
+
+    def make_net_wider(self, new_hidden_dim):
+        """
+        Method to make the recurrent net wider by growing the original hidden dim to the
+        size new_hidden_dim
+        Args:
+             new_hidden_dim: Size of the hidden dim of the recurrent net after making it wider
+        """
+
+        initial_hidden_dim = self._Cells[0]._W_x2i.shape[1]
+        indices_to_copy = np.random.randint(initial_hidden_dim, size=(new_hidden_dim - initial_hidden_dim))
+        replication_factor = np.bincount(indices_to_copy)
+
+        # Growing all the RNN cells
+        self._Cells[0].make_cell_wider(new_hidden_dim=new_hidden_dim,
+                                       indices_to_copy=indices_to_copy,
+                                       replication_factor=replication_factor,
+                                       is_first_cell=True)
+
+        for cell in self._Cells[1:]:
+            cell.make_cell_wider(new_hidden_dim=new_hidden_dim,
+                                 indices_to_copy=indices_to_copy,
+                                 replication_factor=replication_factor,
+                                 is_first_cell=False)
+
+        # Growing the parameters of the recurrent net
+        self._layer_size = new_hidden_dim
+
+        student_w = make_weight_wider_at_input(teacher_w=self._W_h2o.data.numpy(),
+                                               indices_to_copy=indices_to_copy,
+                                               replication_factor=replication_factor)
+        self._W_h2o.data = torch.from_numpy(student_w)
+
+        # Growing the hidden state vectors
+        for h in self._h_prev:
+            for key, param in h.items():
+                student_b = make_h_wider(teacher_b=param.data.numpy(),
+                                         indices_to_copy=indices_to_copy)
+                h[key].data = torch.from_numpy(student_b)
