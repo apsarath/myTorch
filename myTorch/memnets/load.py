6d57517c082b115af57abf2d916dd53f18a85487
apsarath <sarathcse2008@gmail.com>
2018-05-15 20:08:55 -0400

    pmnist task

--- myTorch/memnets/load.py
+++ myTorch/memnets/load.py
@@ -7,17 +7,25 @@ from myTorch.memnets.train import create_experiment
 def load_experiment(save_dir):
 
     config = MyContainer()
-    file_name = os.path.join(save_dir, "config.p")
+    file_name = os.path.join(save_dir, "current", "config.p")
     config.load(file_name)
+
+    print(config)
 
     if config.device != "cpu":
         config.device = "cpu"
+
+    #config.device = "cuda:0"
 
     if config.use_tflogger:
         config.use_tflogger = False
 
     if config.batch_size != 1:
         config.batch_size = 1
+
+    config.save_dir = save_dir
+
+    print(config)
 
     experiment, model, data_iterator, tr, logger, device = create_experiment(config)
 
